53a5076f995024f5e8d47d126f92b5971e668165
khs <hskang200@gmail.com>
2023-05-29 00:11:38 +0900

    jwt 리액트에서 받아오기
    jwt decode 패키지 추가,

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -2,16 +2,29 @@ import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import axios from "axios";
 
-
-const GoogleLoginButton = ({loginFlag}) => {
+const GoogleLoginButton = ({ loginFlag }) => {
   const clientId = process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID;
-
+  const apiEndpointDEV = "http://localhost:4000/login";
+  const sendResToServer = async (res) => {
+    const response = await axios.post(
+      apiEndpointDEV,
+      { clientId: res.clientId },
+      {
+        headers: {
+          Authorization: `Bearer ${res.credential}`,
+        },
+        withCredentials: true,
+      }
+    );
+    console.log(response)
+  };
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <GoogleLogin
         onSuccess={(res) => {
           console.log(res);
+          sendResToServer(res);
           loginFlag(true);
         }}
         onFailure={(err) => {
