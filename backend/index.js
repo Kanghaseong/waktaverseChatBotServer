085155ac7538db7003fcbd558e2936cd1beb90a3
khs <hskang200@gmail.com>
2023-05-31 18:18:53 +0900

    고세구 학습데이터 및 서버코드 수정
    서버코드는 사용자가 고세구와 대화하는 맥락을 이어나가기위해 배열 push를 통한 전달 배열 로직 수정

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 4000;
 const jwt_decode = require('jwt-decode');
 
+let totalToken = 0;
+let Gosegu = "";
+
+
 app.use(helmet());
 app.use(
   cors({
@@ -24,8 +28,6 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-let Gosegu = "";
-
 const filePath = path.join(__dirname, "goseguText.txt");
 
 if (fs.existsSync(filePath)) {
@@ -34,24 +36,33 @@ if (fs.existsSync(filePath)) {
       console.error(err);
       return;
     }
-    Gosegu = data;
+    Gosegu = data; 
+    
+    
   });
 } else {
   console.error("File not found: goseguText.txt");
-}
+} 
+
+var chatArray =  [
+  { role: "system", content: Gosegu },
+];
 
 app.post("/chat", async (req, res) => {
+  
   const inputValue = req.body.inputValue;
+ 
+  chatArray.push({role: "user", content : `${inputValue}`})
   const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: /*Gosegu*/"너는 비서야." },
-      { role: "user", content: `${inputValue}` },
-    ],
+    model: "gpt-3.5-turbo", 
+    temperature:1,
+    max_tokens: 1200,
+    messages: chatArray,
   });
-  console.log(completion.data.choices[0].message.content);
-  console.log(req.body.inputValue);
-  console.log(completion)
+  totalToken += completion.data.usage.total_tokens
+  console.log(totalToken)
+  chatArray.push({ role: "assistant", content : `${completion.data.choices[0].message.content}`})
+  console.log(chatArray)
   res.send(completion.data.choices[0].message.content);
 });
 
